090b45450a652ec1f2e40ff4936f3ed0ab14cdf3
Mai <jea989@gmail.com>
2022-11-28 14:55:42 -0500

    useragent blocked. updated

--- gen-links.py
+++ gen-links.py
@@ -3,8 +3,16 @@ import sys
 from time import sleep
 from bs4 import BeautifulSoup
 
+# CONFIRMED BLOCKED USERAGENTS 
+# 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0'
+
+
+# https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+
+
 s = requests.Session()
-s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0'})
+s.headers.update({'User-Agent': UA})
 r = s.get(sys.argv[1])
 soup = BeautifulSoup(r.text, 'html.parser')
 buttons_list = soup.find_all(class_ = "download")
@@ -12,6 +20,11 @@ for b in buttons_list:
   first_link_code = b['onclick'].split("'")[1]
   first_link = "http://filecrypt.co/Link/" + first_link_code + '.html'
   first_link_resp = s.get(first_link)
+
+  if first_link_resp.status_code == 404:
+    print('ERROR 404: probably need to update User-Agent')
+    exit()
+
   soup = BeautifulSoup(first_link_resp.text, 'html.parser')
   second_link = soup.find("script").text.split("'")[1]
   second_link_resp = s.get(second_link)
--- readme.md
+++ readme.md
@@ -7,4 +7,9 @@ git clone https://github.com/mansuf/zippyshare-downloader
 wget https://raw.githubusercontent.com/mai-gh/filecrypt-list-links/main/gen-links.py
 python gen-links.py https://filecrypt.co/Container/XXXXXXXXXX | tee link-list.txt
 cat link-list.txt | while read line; do PYTHONPATH=$(pwd)/zippyshare-downloader python -m zippyshare_downloader $line; done
+
+or even
+
+python gen-links.py https://filecrypt.co/Container/XXXXXXXXXX | while read line; do PYTHONPATH=$(pwd)/zippyshare-downloader python -m zippyshare_downloader $line; done
+
 ```
